bf0ede8f50987b92b2e1492caa74e209c33befd7
Felbit <140262666+Felbit80@users.noreply.github.com>
2025-04-19 20:52:31 -0300

    Conflitos de Merge ajustados

--- app/routes/userRoutes.js
+++ app/routes/userRoutes.js
@@ -1,21 +1,9 @@
 const express = require("express");
-<<<<<<< Updated upstream
 const router = express.Router();
 const path = require("path");
 
 router.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../views/index.html"));
 });
-=======
-const path = require("path");
-const router = express.Router();
-
-router.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "../views/index.html"));
-  });
-router.get("/createTask", (req, res) => {
-    res.sendFile(path.join(__dirname, "../views/createTask.html"));
-  });
->>>>>>> Stashed changes
 
 module.exports = router;
